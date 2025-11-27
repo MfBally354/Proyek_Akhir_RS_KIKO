9bad8c25cb76faf70cbb071f763cf97dd1e65101
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-27 14:39:50 +0700

    Update Dokter.php

--- app/Models/Dokter.php
+++ app/Models/Dokter.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Dokter extends Model
+return new class extends Migration
 {
-    //
-}
+    public function up()
+    {
+        Schema::create('dokter', function (Blueprint $table) {
+            $table->id('id_dokter');
+            $table->string('nama_dokter', 100);
+            $table->string('spesialisasi', 100)->nullable();
+            $table->unsignedBigInteger('id_poli');
+            $table->timestamps();
+
+            $table->foreign('id_poli')->references('id_poli')->on('poliklinik')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('dokter');
+    }
+};
