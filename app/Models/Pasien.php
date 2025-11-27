2254c015a599742a80d5cd913a84b75e2b0d2472
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-27 14:16:51 +0700

    Update Pasien.php
    Perubahan kecil

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Pasien extends Model
+return new class extends Migration
 {
-    //
-}
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->id('id_pasien');
+            $table->string('nomor_rm', 20);
+            $table->string('nama_pasien', 100);
+            $table->date('tgl_lahir');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pasien');
+    }
+};
