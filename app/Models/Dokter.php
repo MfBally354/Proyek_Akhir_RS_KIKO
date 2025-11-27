7aa6df949bd9cd096b8d11284307822b9638d1af
imti-red <974imti23@gmail.com>
2025-11-27 16:20:38 +0700

    Update: Relasi dokter dan jadwal

--- app/Models/Dokter.php
+++ app/Models/Dokter.php
@@ -1,10 +1,8 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
 class Dokter extends Model
 {
-    //
+    protected $fillable = ['nama','spesialis','kontak'];
+
+    public function jadwal() {
+        return $this->hasMany(Jadwal::class);
+    }
 }
--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -1,10 +1,8 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
 class Jadwal extends Model
 {
-    //
+    protected $fillable = ['dokter_id','hari','jam_mulai','jam_selesai'];
+
+    public function dokter() {
+        return $this->belongsTo(Dokter::class);
+    }
 }
