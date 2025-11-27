b0d88f53dccea8bf57a26965b917a46bff21d312
senoadji1 <spaceimpact124@gmail.com>
2025-11-27 17:22:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PasienController; // Panggil controllernya
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk Halaman Daftar Pasien
+Route::get('/pasien', [PasienController::class, 'index'])->name('pasien.index');
