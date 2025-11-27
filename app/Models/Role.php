d8a8255633a4a91e1a7548ad83d2c8e85637e343
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-27 14:36:27 +0700

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Role extends Model
+return new class extends Migration
 {
-    //
-}
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('id_role');
+            $table->string('nama_role', 50);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
