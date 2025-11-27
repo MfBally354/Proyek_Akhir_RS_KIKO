7a597a3e08ceaaf66c6ec7bea11188319ed9c9a1
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-27 16:23:18 +0700

    perubahan fitur saya

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@include('components.navbar')
+
+<div style="width:100%; height:100vh; background:url('/img/hospital-banner.jpg'); background-size:cover;">
+    <div style="padding:150px; color:white;">
+        <h1 style="font-size:40px;">SELAMAT DATANG DI MYMEDICAL</h1>
+        <p>Layanan kesehatan daring selama 24 jam</p>
+
+        <a href="/login" style="padding:12px 25px; background:#2196f3; color:white; border-radius:5px; text-decoration:none;">
+            Buat Janji Temu
+        </a>
+    </div>
+</div>
+
