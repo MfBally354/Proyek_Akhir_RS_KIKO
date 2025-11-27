dd77a87114985cc00ca79d7ece3d037f1bdd8c3c
Iqbal Guntur <iqbalguntur313@gmail.com>
2025-11-27 16:13:36 +0700

    Update web.php
    ubah dan tambahkan hal baru lah pokoknya

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,14 @@ Route::get('/pasien/{id}/rekam-medis', [RekamMedisController::class, 'index'])->
 Route::get('/pasien/{id}/rekam-medis/create', [RekamMedisController::class, 'create'])->name('rekam.create');
 Route::post('/pasien/{id}/rekam-medis', [RekamMedisController::class, 'store'])->name('rekam.store');
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+
+// Halaman Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+// Login Tenaga Medis
+Route::get('/login-medical', [AuthController::class, 'loginMedical'])->name('login.medical');
+
+// Beranda / Landing Page
+Route::get('/home', [HomeController::class, 'index'])->name('home');
