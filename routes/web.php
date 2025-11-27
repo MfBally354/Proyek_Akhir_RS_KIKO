da2eea4f5245f2e7df16e16d165e3df93acb69d9
imti-red <974imti23@gmail.com>
2025-11-27 16:23:11 +0700

    Update: Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
-<?php
+Route::middleware(['auth'])->group(function() {
+    Route::resource('dokter', DokterController::class);
+    Route::resource('jadwal', JadwalController::class);
+});
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['role:admin'])->group(function() {
+    Route::resource('dokter', DokterController::class);
+    Route::resource('jadwal', JadwalController::class);
 });
